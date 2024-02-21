adc404290f653f893a1157372fea087eaa2da1ad
DanielDeanGithub <github-new@inboxrefresh.com>
2024-02-21 11:06:36 +0000

    Added price setter to menu object

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const menu = {
     _meal: '',
     _price: 0,
-
     set meal(mealToCheck) {
         if (typeof mealToCheck === 'string') return this._meal = mealToCheck;
+    },
+    set price(priceToCheck) {
+        if (typeof priceToCheck === 'number') return this._meal = priceToCheck;
     }
 };
